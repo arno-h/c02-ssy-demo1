091c4bfc76d5214b3c851335fe44f4ac1b4f502b
Arno Hollosi <arno.hollosi@campus02.at>
2021-05-28 13:18:28 +0200

    Routen für Taschenrechner

--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,44 @@ function schoko(request, response) {
   response.send(schoko_art + "schokolade");
 }
 
+router.get('/plus', plus);
+
+// .../plus?param1=17&param2=3
+function plus(req, res) {
+  const param1 = parseFloat(req.query.param1);
+  const param2 = parseFloat(req.query.param2);
+  const result = {
+    result: param1 + param2,
+    info: 'doppelt: _"_ '
+  };
+  res.json(result);
+}
+// {"result":20,"info":"doppelt: _\"_ "}
+
+
+router.get('/sinus', sinus);
+
+// .../sinus?param1=45
+function sinus(req, res) {
+  const param1_degree = parseFloat(req.query.param1);
+  const param1_rad = param1_degree / 360 * 2 * Math.PI;
+  const result = {
+    result: Math.sin(param1_rad)
+  };
+  res.json(result);
+}
+
+
+router.get('/div', div);
+
+function div(req, res) {
+  const param1 = parseFloat(req.query.param1);
+  const param2 = parseFloat(req.query.param2);
+  const result = {
+    result: param1 / param2
+  };
+  res.json(result);
+}
+
+
 module.exports = router;
