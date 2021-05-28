0b47c20bffdca6975ea536383c04789845a5eb74
Arno Hollosi <arno.hollosi@campus02.at>
2021-05-28 13:17:19 +0200

    Erste Route mit Query-Parameter

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,18 @@ const router = express.Router();
 router.get('/', hello);
 
 function hello(req, res) {
-  res.send('NodeJS + Express läuft!');
+  res.send('NodeJS + Express läuft rund & schnell!!');
+}
+
+router.get('/schoko', schoko);
+
+// http://localhost:3000/schoko
+// http://localhost:3000/schoko/milch
+// http://localhost:3000/schoko?art=milch
+
+function schoko(request, response) {
+  const schoko_art = request.query.art;
+  response.send(schoko_art + "schokolade");
 }
 
 module.exports = router;
