60d425bf4bf9066b09607a2b9c5e3ba9ea89afdf
Arno Hollosi <arno.hollosi@campus02.at>
2021-07-06 12:00:31 +0200

    Schritt 7: sum-legs nur für bestimmte Farben

--- services/users/router-statistics.js
+++ services/users/router-statistics.js
@@ -3,11 +3,25 @@ const router = express.Router();
 const database = require('./database');
 
 router.get('/sum-legs', sumLegs)
+router.get('/sum-legs/:color', coloredSumLegs);
 
 const userCollection = database.getCollection('users');
 
 function sumLegs(req, res) {
     let allUsers = userCollection.find();
+    let sumLegs = 0;
+    for (let user of allUsers) {
+        sumLegs += user.legs;
+    }
+    res.json({
+        users: allUsers.length,
+        sum_legs: sumLegs,
+        avg_legs: sumLegs / allUsers.length
+    });
+}
+
+function coloredSumLegs(req, res) {
+    let allUsers = userCollection.find({color: req.params.color});
     let sumLegs = 0;
     for (let user of allUsers) {
         sumLegs += user.legs;
